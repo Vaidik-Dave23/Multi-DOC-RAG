03b6c909f4eb07b21ff80a767e8df42ef9641333
23f3000994 <davevaidik20@gmail.com>
2026-06-19 17:50:58 +0530

    application complited

--- Backend/app/routes/upload.py
+++ Backend/app/routes/upload.py
@@ -1,7 +1,7 @@
 import uuid
 from fastapi import APIRouter, UploadFile, File
 from app.utils.pdf import extract_text
-from app.utils.chunking import hierarchical_chunk
+from app.utils.chunking import naive_chunk
 from app.utils.embedding import embed_and_store
 from app.store import save_store
 
@@ -12,7 +12,7 @@ async def upload(file: UploadFile = File(...)):
     doc_id = str(uuid.uuid4())
     contents = await file.read()
     text = extract_text(contents)
-    chunks = hierarchical_chunk(text)
+    chunks = naive_chunk(text)
     embed_and_store(chunks, doc_id, file.filename)
     save_store()
     return {"message": "Document uploaded successfully", "doc_id": doc_id}
